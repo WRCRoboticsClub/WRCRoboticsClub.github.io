b0edd2e97e6293fb6739401131fd1b78456bfd7f
wrcrobotics <132082542+wrcrobotics@users.noreply.github.com>
2026-06-07 21:15:04 +0545

    Update committee.js

--- src/pages/committee.js
+++ src/pages/committee.js
@@ -95,25 +95,30 @@ export default function Committee({ committeeData = {} }) {
 // -----------------------------
 // Data fetching
 // -----------------------------
-export async function getStaticProps() {
-  try {
-    const res = await fetch(
-      "https://wrcrobotics.pythonanywhere.com/committee"
-    );
+// export async function getStaticProps() {
+//   try {
+//     const res = await fetch(
+//       "https://wrcrobotics.pythonanywhere.com/committee"
+//     );
 
-    const committeeData = await res.json();
+//     const committeeData = await res.json();
 
-    return {
-      props: { committeeData },
-      revalidate: 10,
-    };
-  } catch (err) {
-    return {
-      props: { committeeData: {} },
-      revalidate: 10,
-    };
-  }
-}
+//     return {
+//       props: { committeeData },
+//       revalidate: 10,
+//     };
+//   } catch (err) {
+//     return {
+//       props: { committeeData: {} },
+//       revalidate: 10,
+//     };
+//   }
+// }
+useEffect(() => {
+  fetch("https://wrcrobotics.pythonanywhere.com/committee")
+    .then(res => res.json())
+    .then(setData);
+}, []);
 
 // -----------------------------
 // Styles (cleaned)
