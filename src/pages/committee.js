7c85dd85ade0ad1d9de71c82e705f7a72223d1aa
wrcrobotics <132082542+wrcrobotics@users.noreply.github.com>
2026-06-09 14:33:24 +0545

    Update committee.js

--- src/pages/committee.js
+++ src/pages/committee.js
@@ -1,4 +1,4 @@
-/** @jsxImportSource @theme-ui/core */
+/** @jsxImportSource @theme-ui/core  */
 import { Container, Grid } from "theme-ui";
 import SectionHeader from "../components/section-header";
 import TeamCard from "../components/team-card";
